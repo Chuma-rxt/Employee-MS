2425b19f98b9265743d211b895640addd3e8b2cf
Chuma Raxothi <141155118+Chuma-rxt@users.noreply.github.com>
2024-04-11 14:10:11 +0200

    managing dashboard

--- EmployeeMS/src/Components/Dashboard.jsx
+++ EmployeeMS/src/Components/Dashboard.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+        <div>
+            <div>
+                <div>
+                    <Link to="/dashboard">CWC</Link>
+                    <ul>
+                        <li>
+                            <Link to="/dashboard">Dashboard</Link>
+                        </li>
+                        <li>
+                            <Link to="/dashboard">Manage Employees</Link>
+                        </li>
+                        <li>
+                            <Link to="/dashboard">Category</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- Server/Routes/AdminRoutes.js
+++ Server/Routes/AdminRoutes.js
@@ -1,13 +1,13 @@
-import express from 'express'
-import con from '../utils/db.js'
-import jwt from 'jsonwebtoken'
+import express from 'express';
+import con from '../utils/db.js';
+import jwt from "jsonwebtoken";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/adminlogin', (req, res) => {
-    const sql = "SELCT * from admin Where email = ? and password = ?"
+router.post("/adminlogin", (req, res) => {
+    const sql = "SELECT * from admin Where email = ? and password = ?";
     con.query(sql, [req.body.email, req.body.password], (err, result) => {
-        if (err) return res.json({ loginStatus: false, Error: "Query error" })
+        if (err) return res.json({ loginStatus: false, Error: "Query error" });
         if (result.length > 0) {
             const email = result[0].email;
             const token = jwt.sign(
@@ -16,11 +16,11 @@ router.post('/adminlogin', (req, res) => {
                 { expiresIn: "1d" }
             );
             res.cookie('token', token)
-            return res.json({ loginStatus: true })
+            return res.json({ loginStatus: true });
         } else {
-            return res.json({ loginStatus: false, Error: "wrong email or password" })
+            return res.json({ loginStatus: false, Error: "wrong email or password" });
         }
-    })
+    });
 })
 
 export { router as adminRouter }
